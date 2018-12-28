b04eac2194455ddba3ba55a339bf0314008e249f
ZYJ-JMF <zyjjmf323@126.com>
2018-12-28 21:01:03 +0800

    main.cpp

---
+++ main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <fstream>
+#include <unistd.h>
+#include <string>
+#include "codeGenerator.h"
+
+using namespace std;
+
+extern FILE* yyin;
+extern BlockExprNode* root;
+extern int yyparse()
+
+int main(int argc,char **argv) {
+    char *filename;
+    if (argc == 2)
+        filename = argv[1];
+    else
+        return 0;
+    yyin = fopen(filename,"r");
+    if(!yyin){
+        cout<<"File opening failed\n"<<endl;        
+    }
+    if(yyparse()){
+        cout<<"error"<<endl;
+    }
+    GeneratorContext Pcontext;
+    cout << "Generating Python code" << endl;
+    cout << "----------------------" << endl;
+    context.CodeGen(*root);
+    cout << "----------------------" << endl;
+    cout << "Finished" << endl;
+    char *outputFilename = "output.py" 
+    ofstream outfile;
+    outfile.open(outputFilename, ios::out);
+    context.OutputCode(outfile);
+    outfile.close();
+
+    fclose(yyin);
+    return 0;
+}
